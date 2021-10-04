89c472ef8bbfb0e662e1ad658a8e3bcb1a47809e
2000031041-ShivaChary <2000031041@kluniversity.in>
2021-10-04 23:22:30 +0530

    initial commit

---
+++ src/components/FunctionClick.js
@@ -0,0 +1,10 @@
+function FunctionClick()
+{
+   function  clickbutton(){
+        alert("Thank You For Using Counter")
+    }
+    return(
+    <button onClick={clickbutton}>Help</button>
+    )
+}
+export default FunctionClick
